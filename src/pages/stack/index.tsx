2af439c4f76c2e111cc5bfb901fb7be2165e5dc4
ed <allgoodata@outlook.com>
2024-01-25 16:52:01 +0200

    stack page

--- src/pages/stack/index.tsx
+++ src/pages/stack/index.tsx
@@ -1,10 +1,18 @@
 import Layout from '@/app/components/layout';
+import { icons } from './data';
 
 export default function Stack() {
   return (
     <Layout>
       <main>
-        <section>Stack page</section>
+        <section className="font-bold text-5xl mb-[30px]">Tech stack</section>
+        <section className="w-[75%] m-auto flex flex-wrap justify-center">
+          {icons.map((x, idx) => (
+            <div className="w-[25%] p-[20px] flex justify-center" key={idx}>
+              {<x.icon className="w-[72px] h-[72px]" />}
+            </div>
+          ))}
+        </section>
       </main>
     </Layout>
   );
