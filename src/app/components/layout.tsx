9a73eed31cd2972fac7b776e1db7978f23ed73ba
ed <allgoodata@outlook.com>
2024-01-26 15:55:33 +0200

    changing component casing

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -1,7 +1,7 @@
 // import type { Metadata } from 'next';
 import { JetBrains_Mono } from 'next/font/google';
 import '../globals.css';
-import NavBar from './navbar';
+import NavBar from './NavBar';
 
 const inter = JetBrains_Mono({ subsets: ['latin'] });
 
