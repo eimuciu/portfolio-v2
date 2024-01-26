38021e31df5b2e1c452eaf7e5123ed39223c2255
ed <allgoodata@outlook.com>
2024-01-26 19:29:11 +0200

    fixing navigation to the top

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -15,17 +15,22 @@ export default function NavBar() {
 
   return (
     <main>
-      <section className="flex flex-row py-[10px] px-[20px] bg-[#2B303A] text-white md:flex-col md:px-[10px] md:relative">
+      <div
+        onClick={openMenu}
+        className="hidden fixed right-[25px] z-10 bottom-[25px] text-[#2B303A] md:block"
+      >
+        <MdOutlineRestaurantMenu className="w-[50px] h-[50px]" />
+      </div>
+      <div className="basis-[20%] invisible text-4xl font-bold py-[10px] px-[20px] md:basis-[100%] md:flex md:justify-center md:relative">
+        <Link onClick={closeMenu} href="/">
+          eimis.codes
+        </Link>
+      </div>
+      <section className="flex fixed flex-row top-0 w-[100%] z-10 py-[10px] px-[20px] bg-[#2B303A] text-white md:flex-col md:px-[10px]">
         <div className="basis-[20%] text-4xl font-bold md:basis-[100%] md:flex md:justify-center md:relative">
           <Link onClick={closeMenu} href="/">
             eimis.codes
           </Link>
-          <div
-            onClick={openMenu}
-            className="hidden absolute right-0 top-[150%] text-[#2B303A] md:block"
-          >
-            <MdOutlineRestaurantMenu />
-          </div>
         </div>
         <div
           className={`basis-[80%] gap-x-[30px] flex justify-center items-center text-xl md:basis-[100%] md:pt-[15px] md:pb-[15px] md:text-lg	md:text-base md:absolute md:bg-[#2B303A] md:left-0 md:top-[100%] md:w-[100%] md:gap-y-[15px] md:z-10 md:${
