c814c1eb3808b6e4b23e15e7e4bdb007aae85a8c
ed <allgoodata@outlook.com>
2024-01-26 16:33:52 +0200

    navigation font size

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link';
 export default function NavBar() {
   return (
     <main>
-      <section className="flex flex-row py-[10px] px-[20px] bg-[#2B303A] text-white md:flex-col">
+      <section className="flex flex-row py-[10px] px-[20px] bg-[#2B303A] text-white md:flex-col md:px-[10px]">
         <div className="basis-[20%] text-4xl font-bold md:basis-[100%] md:flex md:justify-center ">
           <Link href="/">eimis.codes</Link>
         </div>
-        <div className="basis-[80%] gap-x-[30px] flex justify-center items-center text-xl md:basis-[100%] md:pt-[15px] md:pb-[5px] md:text-lg md:gap-x-[10px]">
+        <div className="basis-[80%] gap-x-[30px] flex justify-center items-center text-xl md:basis-[100%] md:pt-[15px] md:pb-[5px] md:text-lg md:gap-x-[0px] md:justify-between	sm:text-base">
           <Link className="" href="/">
             Home
           </Link>
